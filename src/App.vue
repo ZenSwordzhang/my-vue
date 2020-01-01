0a3d775cafab80c0873699610f952482be083dfd
zsx <18273117003@163.com>
2020-01-01 20:25:28 +0800

    vue项目构建修改

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,18 @@
                 console.log(root.value); // <div/>
             });
 
+            // 应该总是将 ref 存放到 reactive 对象中
+            const m = reactive({
+                count: ref(0),
+            });
+            console.log(m.count === 0);
+            const arr = reactive({
+                list: [m],
+            });
+            console.log(arr.list[0].count === 0);
             return {
-                root,
+                root
             };
-        },
+        }
     };
 </script>
