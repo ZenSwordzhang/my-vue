c5f7667421ba91b024f1e4096e81089e96a20846
zsx <18273117003@163.com>
2020-01-13 20:27:44 +0800

    角色管理

--- src/views/layout/management/role/components/Main.vue
+++ src/views/layout/management/role/components/Main.vue
@@ -1,26 +1,6 @@
 <template>
     <div id="main-wrap">
         <div class="content">
-           <!-- <div class="menu-content">
-                <el-row :gutter="20">
-                    <el-col :span="8">
-                        <div class="grid-content bg-purple">
-                            <div style="margin-top: 15px;">
-                                <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
-                                    <div slot="append">
-                                        <el-select v-model="select" placeholder="请选择">
-                                            <el-option label="餐厅名" value="1"></el-option>
-                                            <el-option label="订单号" value="2"></el-option>
-                                            <el-option label="用户电话" value="3"></el-option>
-                                        </el-select>
-                                        <el-button icon="el-icon-search" class="el-search"></el-button>
-                                    </div>
-                                </el-input>
-                            </div>
-                        </div>
-                    </el-col>
-                </el-row>
-            </div>-->
             <div class="main-content">
                 <router-view/>
             </div>
--- src/views/management/role/Index.vue
+++ src/views/management/role/Index.vue
@@ -1,26 +1,74 @@
 <template>
-    <div>
-        <el-container>
-            <el-aside class="content-aside">Aside</el-aside>
-            <el-main class="content-main">Main</el-main>
-        </el-container>
+    <div id="main-wrap">
+        <el-row>
+            <el-col :span="5" class="content-aside">
+                <div class="grid-content bg-purple">
+                    <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
+                        <div slot="append">
+                            <el-select v-model="select" placeholder="请选择">
+                                <el-option label="餐厅名" value="1"></el-option>
+                                <el-option label="订单号" value="2"></el-option>
+                                <el-option label="用户电话" value="3"></el-option>
+                            </el-select>
+                            <el-button icon="el-icon-search" class="el-search"></el-button>
+                        </div>
+                    </el-input>
+                </div>
+                <div class="role-menu">abc</div>
+            </el-col>
+            <el-col :span="18">
+                <div class="grid-content bg-purple-light"></div>
+                <div class="grid-content bg-purple-light">ccc</div>
+            </el-col>
+        </el-row>
     </div>
 </template>
 <script>
     import {ref, reactive} from "@vue/composition-api";
 
     export default {
-        setup() {}
+        setup() {
+            const input3 = ref("");
+            const select = ref("");
+            return {
+                input3, select
+            };
+        }
     }
 </script>
 <style lang="scss" scoped>
-    .content-aside {
-        display: block;
-        width: 200px;
-        border-right: 1px red;
-    }
-    .content-main {
-        display: block;
-        width: 200px;
+
+    #main-wrap {
+        position: absolute;
+        left: 15px;
+        top: 10px;
+        right: 0;
+        bottom: 0;
+
+        .el-row, .content-aside, .role-menu {
+            height: 100%;
+        }
+        .role-menu {
+            background-color: #D3DCE6;
+        }
+        .input-with-select {
+            width: 85%;
+        }
+
+        .el-search {
+            position: absolute;
+            left: 65px;
+            background-color: #337AB7;
+        }
+
+        .operation-button {
+            padding-top: 15px;
+            .delete-button {
+                padding-left: 10px;
+            }
+        }
+        .grid-content .bg-purple-light {
+            height: 50%;
+        }
     }
 </style>
