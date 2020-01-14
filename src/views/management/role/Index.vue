6d96e13af31981cb6243593ee875c22825c879ca
zsx <18273117003@163.com>
2020-01-14 11:25:53 +0800

    角色管理

--- src/views/management/role/Index.vue
+++ src/views/management/role/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="main-wrap">
-        <el-row>
+        <el-row class="content-left">
             <el-col :span="5" class="content-aside">
                 <div class="grid-content bg-purple">
                     <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
@@ -14,11 +14,62 @@
                         </div>
                     </el-input>
                 </div>
-                <div class="role-menu">abc</div>
+                <div class="role-menu">
+                    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+                </div>
             </el-col>
-            <el-col :span="18">
-                <div class="grid-content bg-purple-light"></div>
-                <div class="grid-content bg-purple-light">ccc</div>
+            <el-col :span="18" class="content-main">
+                <div class="role-info-upside">
+                    <el-row class="role-title">角色信息：</el-row>
+                    <el-row class="role-title">
+                        <el-col :span="4" class="content-aside">角色编码：xxxx</el-col>
+                        <el-col :span="4" class="content-aside">角色名称：xxxx</el-col>
+                        <el-col :span="4" class="content-aside">创建时间：xxxx</el-col>
+                        <el-col :span="4" class="content-aside">创建者：xxxx</el-col>
+                        <el-col :span="4" class="content-aside">状态：正常/待删除</el-col>
+                        <el-col :span="4" class="content-aside">添加用户</el-col>
+                    </el-row>
+                    <el-row class="role-table">
+                        <template>
+                            <el-table
+                                    :data="tableData"
+                                    border
+                                    style="width: 100%">
+                                <el-table-column
+                                        fixed
+                                        type="selection"
+                                        width="80">
+                                </el-table-column>
+                                <el-table-column
+                                        type="index"
+                                        label="序号"
+                                        width="80">
+                                </el-table-column>
+                                <el-table-column
+                                        prop="username"
+                                        label="用户名称"
+                                        width="180">
+                                </el-table-column>
+                                <el-table-column
+                                        prop="role"
+                                        label="归属角色"
+                                        width="180">
+                                </el-table-column>
+                                <el-table-column
+                                        prop="permission"
+                                        label="已有权限">
+                                </el-table-column>
+                            </el-table>
+                        </template>
+                    </el-row>
+                </div>
+                <div class="permission-info-bottom">
+                    <el-row class="permission-title">角色权限：</el-row>
+                    <el-row class="permission-title">
+                        <el-button type="primary" round>分配权限</el-button>
+                    </el-row>
+                    <el-row class="role-title">角色信息：</el-row>
+                </div>
             </el-col>
         </el-row>
     </div>
@@ -30,8 +81,66 @@
         setup() {
             const input3 = ref("");
             const select = ref("");
+            const tableData = reactive([{
+                id: 1,
+                username: '王小虎',
+                role: '财务',
+                permission: '审计'
+            }, {
+                id: 2,
+                username: '王小虎',
+                role: '财务',
+                permission: '审计'
+            }, {
+                username: '王小虎',
+                role: '财务',
+                permission: '审计'
+
+            }]);
+            const data = reactive([{
+                label: '一级 1',
+                children: [{
+                    label: '二级 1-1',
+                    children: [{
+                        label: '三级 1-1-1'
+                    }]
+                }]
+            }, {
+                label: '一级 2',
+                children: [{
+                    label: '二级 2-1',
+                    children: [{
+                        label: '三级 2-1-1'
+                    }]
+                }, {
+                    label: '二级 2-2',
+                    children: [{
+                        label: '三级 2-2-1'
+                    }]
+                }]
+            }, {
+                label: '一级 3',
+                children: [{
+                    label: '二级 3-1',
+                    children: [{
+                        label: '三级 3-1-1'
+                    }]
+                }, {
+                    label: '二级 3-2',
+                    children: [{
+                        label: '三级 3-2-1'
+                    }]
+                }]
+            }]);
+            const defaultProps = reactive({
+                children: 'children',
+                label: 'label'
+            });
+            const handleNodeClick = data => {
+                console.log(data);
+            };
             return {
-                input3, select
+                input3, select, tableData, data, defaultProps, handleNodeClick
             };
         }
     }
@@ -45,12 +154,21 @@
         right: 0;
         bottom: 0;
 
-        .el-row, .content-aside, .role-menu {
+        .content-left {
+            height: 90%;
+        }
+
+        .content-aside, .role-menu, .content-main {
             height: 100%;
         }
+
         .role-menu {
             background-color: #D3DCE6;
+            .el-tree {
+                background-color: #D3DCE6;
+            }
         }
+
         .input-with-select {
             width: 85%;
         }
@@ -63,12 +181,45 @@
 
         .operation-button {
             padding-top: 15px;
+
             .delete-button {
                 padding-left: 10px;
             }
         }
-        .grid-content .bg-purple-light {
-            height: 50%;
+
+        .content-main {
+            padding-left: 15px;
+
+            .role-info-upside {
+                height: 300px;
+
+                .role-title {
+                    margin-top: 10px;
+                    margin-bottom: 10px;
+                    height: 40px;
+                }
+
+                .role-table {
+                    height: 100%;
+                }
+            }
+
+            .permission-info-bottom {
+                padding-top: 20px;
+                height: 65%;
+
+                .permission-title {
+                    padding-top: 10px;
+                    padding-bottom: 10px;
+                    height: 55px;
+
+                    .el-button {
+                        float: right;
+                        margin-top: 0;
+                        margin-right: 20px;
+                    }
+                }
+            }
         }
     }
 </style>
