bc71c3fc33aed6c2155a09601faf133340e27bb6
zsx <18273117003@163.com>
2020-01-17 10:03:10 +0800

    角色管理

--- src/views/management/role/Index.vue
+++ src/views/management/role/Index.vue
@@ -70,27 +70,30 @@
                     </el-row>
                     <el-row class="permission-content">
                         <el-collapse v-model="activeNames" @change="handleChange">
-                            <el-collapse-item title="一致性 Consistency" name="1">
-                                <template>
-                                    <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
-                                    <div style="margin: 15px 0;"></div>
-                                    <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
-                                        <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
-                                    </el-checkbox-group>
-                                </template>
+                            <el-collapse-item title="组织人事管理" name="1">
+                                <el-collapse-item title="组织架构管理 部门管理" name="1-1">
+                                    <template>
+                                        <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+                                        <div style="margin: 15px 0;"></div>
+                                        <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
+                                            <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
+                                        </el-checkbox-group>
+                                    </template>
+                                </el-collapse-item>
+                                <el-collapse-item title="组织架构管理 部门管理" name="1-2">
+                                    <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                                    <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                                </el-collapse-item>
                             </el-collapse-item>
-                            <el-collapse-item title="反馈 Feedback" name="2">
-                                <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-                                <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-                            </el-collapse-item>
-                            <el-collapse-item title="效率 Efficiency" name="3">
-                                <div>简化流程：设计简洁直观的操作流程；</div>
-                                <div>清晰明确：语言表达清晰且表意明确，让用户快速理解进而作出决策；</div>
-                                <div>帮助用户识别：界面简单直白，让用户快速识别而非回忆，减少用户记忆负担。</div>
-                            </el-collapse-item>
-                            <el-collapse-item title="可控 Controllability" name="4">
-                                <div>用户决策：根据场景可给予用户操作建议或安全提示，但不能代替用户进行决策；</div>
-                                <div>结果可控：用户可以自由的进行操作，包括撤销、回退和终止当前操作等。</div>
+                            <el-collapse-item title="模具工时管理" name="2">
+                                <el-collapse-item title="机台管理" name="2-1">
+                                    <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                                    <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                                </el-collapse-item>
+                                <el-collapse-item title="模具工序管理" name="2-2">
+                                    <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
+                                    <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
+                                </el-collapse-item>
                             </el-collapse-item>
                         </el-collapse>
                     </el-row>
@@ -106,12 +109,12 @@
         setup() {
             const input3 = ref("");
             const select = ref("");
-            const activeNames = ref(['1']);
-            const cityOptions = ref(['上海', '北京', '广州', '深圳']);
+            const activeNames = reactive(['1']);
+            const cityOptions = reactive(['上海', '北京', '广州', '深圳']);
             let checkAll = false;
             let checkedCities = ['上海', '北京'];
-            let cities = cityOptions;
-            let isIndeterminate = true;
+            const cities = cityOptions;
+            let isIndeterminate = ref(true);
             const tableData = reactive([{
                 id: 1,
                 username: '王小虎',
@@ -175,12 +178,17 @@
             };
             const handleCheckAllChange =(val)=> {
                 checkedCities = val ? cityOptions : [];
+                console.log(checkedCities);
                 isIndeterminate = false;
+                console.log(isIndeterminate);
             };
             const handleCheckedCitiesChange =(value)=> {
                 let checkedCount = value.length;
-                checkAll = checkedCount === this.cities.length;
-                isIndeterminate = checkedCount > 0 && checkedCount < this.cities.length;
+                console.log(checkedCount);
+                checkAll = checkedCount === cities.length;
+                console.log(checkAll);
+                isIndeterminate = checkedCount > 0 && checkedCount < cities.length;
+                console.log(isIndeterminate);
             };
             return {
                 input3, select, activeNames, cityOptions, checkAll, checkedCities, cities, isIndeterminate, tableData, data, defaultProps,
@@ -197,61 +205,48 @@
         top: 10px;
         right: 0;
         bottom: 0;
-
         .content-left {
             height: 90%;
         }
-
         .content-aside, .role-menu, .content-main {
             height: 100%;
         }
-
         .role-menu {
             background-color: #D3DCE6;
             .el-tree {
                 background-color: #D3DCE6;
             }
         }
-
         .input-with-select {
             width: 85%;
         }
-
         .el-search {
             position: absolute;
             left: 65px;
             background-color: #337AB7;
         }
-
         .operation-button {
             padding-top: 15px;
-
             .delete-button {
                 padding-left: 10px;
             }
         }
-
         .content-main {
             padding-left: 15px;
-
             .role-info-upside {
                 height: 300px;
-
                 .role-title {
                     margin-top: 10px;
                     margin-bottom: 10px;
                     height: 40px;
                 }
-
                 .role-table {
                     height: 100%;
                 }
             }
-
             .permission-info-bottom {
                 margin-top: 20px;
                 height: 65%;
-
                 .permission-title {
                     height: 40px;
                     .el-button {
@@ -259,7 +254,6 @@
                         margin-right: 20px;
                     }
                 }
-
                 .permission-content {
                     margin-top: 10px;
                 }
